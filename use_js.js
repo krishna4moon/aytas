dd01ddcf2579019f4e3a3451ba055826630d347b
Krishna <28984639+krishna4moon@users.noreply.github.com>
2026-02-10 21:59:31 +0530

    Create use_js.js

---
+++ use_js.js
@@ -0,0 +1,11 @@
+<script src="https://cdn.example.com/analytics.js" async></script>
+
+<script>
+// Paste the entire code here
+(function(){/* full code */})();
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!-- Your code injected -->
+
+  
